a32e0fc6d7962ffef91d129f1f6074576c04bd00
Samuel Paes <samuel.paes@propig.com.br>
2021-01-05 23:45:16 -0300

    Sequencia na leitura do livro.

---
+++ Chapter1/named_tuple.py
@@ -0,0 +1,12 @@
+from collections import namedtuple
+
+City = namedtuple('City', 'name country population coordinates')
+tokyo = City('Tokyo', 'JP', 36.933, (35.689722, 139.691667))
+tokyo
+
+LatLong = namedtuple('LatLong', 'lat long')
+delhi_data = ('Delhi NCR', 'IN', 21.935, LatLong(28.613889, 77.208889))
+delhi = City._make(delhi_data)  
+delhi._asdict()  
+for key, value in delhi._asdict().items():
+    print(key + ':', value)
